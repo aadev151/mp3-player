9b0e8602e50f70f1d1eb0d3d3ccaf191ff180c5c
Alex Anisimov <62463175+aadev151@users.noreply.github.com>
2021-11-10 21:50:22 +0100

    Added comments

--- play.py
+++ play.py
@@ -1,3 +1,8 @@
+"""
+The window opens a player
+"""
+
+
 from PyQt5.QtWidgets import QWidget
 from PyQt5 import QtCore, QtMultimedia, uic
 from PyQt5.QtCore import Qt
@@ -12,6 +17,10 @@ from static import play_ui
 class PlayMusic(QWidget, play_ui.Ui_Form):
 
     def __init__(self, filename):
+        """
+        :param filename: directory of the playing track
+        """
+        
         super().__init__()
         self.filename = filename
         self.setupUi(self)
@@ -23,13 +32,18 @@ class PlayMusic(QWidget, play_ui.Ui_Form):
         self.background.setPixmap(QPixmap('static/bg.jpg').scaled(
             self.background.width(), self.background.height()))
         try:
+            # Trying to get the song name and the artist
+            # from ID3 tags
             file = eyed3.load(filename)
             self.name.setText(file.tag.title)
             self.artist.setText(file.tag.artist)
         except AttributeError:
             self.name.setText(filename.split('/')[-1][:-5])
         self.action_button.clicked.connect(self.pause_play)
+        
         try:
+            # Trying to get a cover art
+            # and save it to static/cover.png
             afile = MP3(filename)
             try:
                 with open('static/cover.png', 'wb') as fff:
@@ -42,6 +56,8 @@ class PlayMusic(QWidget, play_ui.Ui_Form):
             self.cover.setPixmap(QPixmap('static/music.png'))
 
     def play(self, filename):
+        """The function starts playing the song"""
+        
         media = QtCore.QUrl.fromLocalFile(filename)
         content = QtMultimedia.QMediaContent(media)
         self.player = QtMultimedia.QMediaPlayer()
@@ -49,6 +65,8 @@ class PlayMusic(QWidget, play_ui.Ui_Form):
         self.player.play()
 
     def pause_play(self):
+        """The function is called when a user taps pause/play button"""
+        
         if self.action_button.text() == '| |':
             self.action_button.setText('►')
             self.player.pause()
@@ -57,9 +75,13 @@ class PlayMusic(QWidget, play_ui.Ui_Form):
             self.player.play()
 
     def keyPressEvent(self, event):
+        """Pressing space is equal to tapping pause/play button"""
+        
         if event.key() == Qt.Key_Space:
             self.pause_play()
 
     def closeEvent(self, event):
+        """Stopping music when the window is closed"""
+        
         self.player.pause()
         event.accept()
